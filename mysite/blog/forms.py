5185853016a0a73881b7c2a1d04ae95200c668dc
healers2004 <88001950+healers2004@users.noreply.github.com>
2024-05-30 20:48:41 +0300

    Add files via upload

---
+++ mysite/blog/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from blog.models import Comment, Post
+from polls.models import Choice
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Your Name"}
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Leave a comment!"}
+        )
+    )
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'categories']  # Ensure 'poll' is a field in your model if you include it
+
+class VoteForm(forms.Form):
+    choice = forms.ModelChoiceField(queryset=Choice.objects.none(), widget=forms.RadioSelect, empty_label=None)
+
+    def __init__(self, question_id, *args, **kwargs):
+        super(VoteForm, self).__init__(*args, **kwargs)
+        self.fields['choice'].queryset = Choice.objects.filter(question_id=question_id)
